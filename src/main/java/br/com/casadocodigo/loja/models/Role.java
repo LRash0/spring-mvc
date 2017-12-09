81711162177d19a30fcd5e9dd22b41a506e3a3f6
Lucas Freirem <lucasdrf92@gmail.com>
2017-12-09 17:16:25 -0300

    Parte de login. Falta criacao de usuario

---
+++ src/main/java/br/com/casadocodigo/loja/models/Role.java
@@ -0,0 +1,21 @@
+package br.com.casadocodigo.loja.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.springframework.security.core.GrantedAuthority;
+
+@SuppressWarnings("serial")
+@Entity
+public class Role implements GrantedAuthority {
+
+    @Id
+    private String name;
+
+    @Override
+    public String getAuthority() {
+
+	return name;
+    }
+
+}
