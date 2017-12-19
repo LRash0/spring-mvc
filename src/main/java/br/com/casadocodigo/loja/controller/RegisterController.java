493d8f1228d1f5d20b5aa7c926fa30c3d793d836
Lucas Freirem <lucasdrf92@gmail.com>
2017-12-18 21:53:19 -0300

    Registration part

---
+++ src/main/java/br/com/casadocodigo/loja/controller/RegisterController.java
@@ -0,0 +1,13 @@
+package br.com.casadocodigo.loja.controller;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Transactional
+@RequestMapping("/register")
+public class RegisterController {
+
+}
